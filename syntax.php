fc3ff9bb93894a988500452d1cdebacaf4bb21ad
Gerrit Uitslag <klapinklapin@gmail.com>
2014-01-16 23:24:39 +0100

    PHPdocs and visibility

--- syntax.php
+++ syntax.php
@@ -15,23 +15,52 @@ if(!defined('DOKU_INC')) die();
  */
 class syntax_plugin_searchform extends DokuWiki_Syntax_Plugin {
 
+    /**
+     * Syntax Type
+     *
+     * Needs to return one of the mode types defined in $PARSER_MODES in parser.php
+     * @return string
+     */
+    public function getType() { return 'substition'; }
 
-    function getType() { return 'substition'; }
-    function getSort() { return 138; }
+    /**
+     * Sort order when overlapping syntax
+     * @return int
+     */
+    public function getSort() { return 138; }
 
-    function connectTo($mode) {
+    /**
+     * @param $mode
+     */
+    public function connectTo($mode) {
 		$this->Lexer->addSpecialPattern('\{searchform\}',$mode,'plugin_searchform');
     }
 
-    function handle($match, $state, $pos, &$handler) {  
+    /**
+     * Handler to prepare matched data for the rendering process
+     *
+     * @param   string       $match   The text matched by the patterns
+     * @param   int          $state   The lexer state for the match
+     * @param   int          $pos     The character position of the matched text
+     * @param   Doku_Handler $handler Reference to the Doku_Handler object
+     * @return  array Return an array with all data you want to use in render
+     */
+    public function handle($match, $state, $pos, &$handler) {
     	return array($match, $state, $pos);
     }
 
-    function render($mode, &$renderer, $data) {
- 		
+    /**
+     * The actual output creation.
+     *
+     * @param   $format   string        output format being rendered
+     * @param   $renderer Doku_Renderer reference to the current renderer object
+     * @param   $data     array         data created by handler()
+     * @return  boolean                 rendered correctly?
+     */
+    public function render($format, &$renderer, $data) {
  		global $lang, $INFO, $ACT, $QUERY;
  		
-		if ($mode == 'xhtml') {
+		if ($format == 'xhtml') {
             // don't print the search form if search action has been disabled
             if(!actionOK('search')) return true;
 
