4f97190a805837df28e47b0233aa6288039aa9a1
Gerrit Uitslag <klapinklapin@gmail.com>
2020-12-05 22:18:15 +0100

    Change global query, so that searchfield at the search page is also mentioning the namespace filter

--- action.php
+++ action.php
@@ -6,9 +6,6 @@
  * @author  Gerrit Uitslag <klapinklapin@gmail.com>
  */
 
-// must be run within Dokuwiki
-if(!defined('DOKU_INC')) die();
-
 /**
  * Class action_plugin_searchform
  */
@@ -21,34 +18,25 @@ class action_plugin_searchform extends DokuWiki_Action_Plugin {
      * @return void
      */
     public function register(Doku_Event_Handler $controller) {
-
-        $controller->register_hook('SEARCH_QUERY_FULLPAGE', 'BEFORE', $this, '_search_query_fullpage');
-        $controller->register_hook('SEARCH_QUERY_PAGELOOKUP', 'BEFORE', $this, '_search_query_pagelookup');
-
+        $controller->register_hook('DOKUWIKI_STARTED', 'AFTER',  $this, 'changeQuery');
     }
 
     /**
-     * Restrict fullpage search to namespace given as url parameter
+     * Restrict the global query to namespace given as url parameter
      *
      * @param Doku_Event $event  event object by reference
      * @param mixed      $param  [the parameters passed as fifth argument to register_hook() when this
      *                           handler was registered]
      * @return void
      */
-    public function _search_query_fullpage(Doku_Event &$event, $param) {
-        $this->_addNamespace2query($event->data['query']);
-    }
+    public function changeQuery(Doku_Event &$event, $param) {
+        global $QUERY;
+        global $ACT;
 
-    /**
-     * Restrict page lookup search to namespace given as url parameter
-     *
-     * @param Doku_Event $event  event object by reference
-     * @param mixed      $param  [the parameters passed as fifth argument to register_hook() when this
-     *                           handler was registered]
-     * @return void
-     */
-    public function _search_query_pagelookup(Doku_Event &$event, $param) {
-        $this->_addNamespace2query($event->data['id']);
+        if($ACT != 'search'){
+            return;
+        }
+        $this->addNamespaceToQuery($QUERY);
     }
 
     /**
@@ -56,7 +44,7 @@ class action_plugin_searchform extends DokuWiki_Action_Plugin {
      *
      * @param string &$query (reference) search query string
      */
-    private function _addNamespace2query(&$query) {
+    private function addNamespaceToQuery(&$query) {
         global $INPUT;
 
         $ns = cleanID($INPUT->str('ns'));
@@ -69,5 +57,3 @@ class action_plugin_searchform extends DokuWiki_Action_Plugin {
     }
 
 }
-
-// vim:ts=4:sw=4:et:
